9d73a624d05dc00a917f90d3fb904efd6cf7bcbe
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-02-26 18:47:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,8 @@ def fizzbuzz(int)
   
   if int % 3 == 0 and int % 5 == 0
     return "FizzBuzz"
+  else
+    return "nil"
   end
   
   if int % 3 == 0
@@ -13,10 +15,6 @@ def fizzbuzz(int)
   
   if int % 5 == 0
     return "Buzz"
-  end
-  
-  if int % 3 != 0 and int % 5 != 0
-    return "nil"
   end
 
 end
